41b79099f0abb94b92735a867447195a37b3ae8b
ooherin <555ohr@naver.com>
2023-05-31 09:51:32 +0900

    feat: 이미지 업로드 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css'
 import MainPage from './page/mainPage'
 import GlobalStyles from './style/global'
+import PostContextProvider from './context/post'
 function App() {
 	return (
 		//globalStyles 사용
 		//mainPage만 렌더링
-		<>
+		<PostContextProvider>
 			<GlobalStyles />
 			<MainPage />
-		</>
+		</PostContextProvider>
 	)
 }
 
